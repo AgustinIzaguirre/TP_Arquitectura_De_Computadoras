524f834856dc2737947c970ae622cc074e238212
AgustinIzaguirre <Agustinizag1@gmail.com>
2017-10-27 17:15:27 -0300

    Bare Bones Plus RTClib

---
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/lib.h
@@ -0,0 +1,23 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <stdint.h>
+
+void * memset(void * destination, int32_t character, uint64_t length);
+void * memcpy(void * destination, const void * source, uint64_t length);
+
+char *cpuVendor(char *result);
+
+int getSeconds(void);
+
+int getMinutes(void);
+
+int getHours(void);
+
+int getYear(void);
+
+int getMonth(void);
+
+int getDay(void);
+
+#endif
